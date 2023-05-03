284bea1d458a6e58eda0c1fdda842252f68507fe
Lockheed Not Martin <122471482+LockheedNotMartin@users.noreply.github.com>
2023-05-03 09:02:40 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const cacheName = 'template-cache';
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/icon.png',
+          '/icon-512x512.png',
+        ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
